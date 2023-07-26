b2af4dc479357486de6387c4924b60853f9dab70
resna <resnapangestu01@gmail.com>
2023-07-26 10:00:02 +0700

    -

--- DS_C_timer.sh
+++ DS_C_timer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for (( i=21; i>0; i--)); do
+for (( i=30; i>0; i--)); do
   sleep 1 &
   printf "Jangan di tutup, sedang proses update. Trims as@epic  $i \r"
   wait
@@ -8,6 +8,6 @@ done
 git pull
 "C:\ds_rsudkotabandung\DesktopService.exe" $1 &
 
-start chrome  https://192.168.4.25/app/Login
+start chrome  https://192.168.4.25:4433/app/Login
 
 
